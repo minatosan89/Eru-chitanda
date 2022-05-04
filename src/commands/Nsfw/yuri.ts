160e27cbcee58206d2308282f70f8dedb4e3e7f6
Ray <89797110+Whatup364@users.noreply.github.com>
2022-05-04 09:49:09 +0530

    Update yuri.ts

--- src/commands/Nsfw/yuri.ts
+++ src/commands/Nsfw/yuri.ts
@@ -2,7 +2,7 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
-import axios from 'axios'
+import akaneko from "akaneko"
 import request from '../../lib/request'
 import { MessageType } from '@adiwajshing/baileys'
 // import { MessageType, Mimetype } from '@adiwajshing/baileys'
@@ -12,7 +12,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'yuri',
             description: `Will send you random nsfw neko image.`,
-            aliases: ['yuri'],
+            aliases: ['femdom'],
             category: 'nsfw',
             usage: `${client.config.prefix}yuri`,
             baseXp: 50
@@ -20,36 +20,40 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        // fetch result of https://waifu.pics/api/nsfw/neko from the API using axios
-        const { data } = await axios.get('https://nekos.life/api/v2/img/yuri')
-        if ( !(await this.client.getGroupData(M.from)).nsfw)
-            return void M.reply(
-                `Don't be a pervert, Baka! This is not an NSFW group.`
-            )
-        const buffer = await request.buffer(data.url).catch((e) => {
-            return void M.reply(e.message)
-        })
-        while (true) {
-            try {
-                M.reply(
-                    buffer || 'Could not fetch image. Please try again later',
-                    MessageType.image,
-                    undefined,
-                    undefined,
-                    `💦\n`,
-                    undefined
-                ).catch((e) => {
-                    console.log(`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
-                    // console.log('Failed')
-                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
-                })
-                break
-            } catch (e) {
-                // console.log('Failed2')
-                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
-                console.log(`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
-            }
-        }
-        return void null
-    }
+		// fetch result of https://nekos.life/api/v2/img/wallpaper from the API using axios
+		const wall = await akaneko.nsfw.yuri();
+		if (!(await this.client.getGroupData(M.from)).nsfw)
+			return void M.reply(
+				`Don't be a pervert, Baka! This is not an NSFW group.`
+			);
+		const buffer = await request.buffer(wall).catch((e) => {
+			return void M.reply(e.message);
+		});
+		while (true) {
+			try {
+				M.reply(
+					buffer || "Could not fetch image. Please try again later",
+					MessageType.image,
+					undefined,
+					undefined,
+					`🌟\n`,
+					undefined
+				).catch((e) => {
+					console.log(
+						`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`
+					);
+					// console.log('Failed')
+					M.reply(`Could not fetch image. Here's the URL: ${wall}`);
+				});
+				break;
+			} catch (e) {
+				// console.log('Failed2')
+				M.reply(`Could not fetch image. Here's the URL : ${wall}`);
+				console.log(
+					`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`
+				);
+			}
+		}
+		return void null;
+	};
 }
