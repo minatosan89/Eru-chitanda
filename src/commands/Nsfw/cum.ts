b955f01e6cce063ac366dabd1c1097f6d97d731e
Ray <89797110+Whatup364@users.noreply.github.com>
2022-05-04 10:15:04 +0530

    Update cum.ts

--- src/commands/Nsfw/cum.ts
+++ src/commands/Nsfw/cum.ts
@@ -2,13 +2,14 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
-
+import { MessageType } from "@adiwajshing/baileys";
+import gen from "images-generator"
+import request from '../../lib/request';
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
 			command: "cum",
-			description: `Just a random gif of cum girl.`,
+			description: `Just a random image of cum girl.`,
 			aliases: ["cum"],
 			category: "nsfw",
 			usage: `${client.config.prefix}cum`,
@@ -17,26 +18,40 @@ export default class Command extends BaseCommand {
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		let caption = "";
-		caption += `🌟`;
+		// fetch result of https://nekos.life/api/v2/img/wallpaper from the API using axios
+		const i = await gen.nsfw.anime.cum()
 		if (!(await this.client.getGroupData(M.from)).nsfw)
 			return void M.reply(
 				`Don't be a pervert, Baka! This is not an NSFW group.`
 			);
-		M.reply(
-			await this.client.util.GIFBufferToVideoBuffer(
-				await this.client.getBuffer(
-					(
-						await this.client.fetch<{ url: string }>(
-							`https://nekos.life/api/v2/img/cum`
-						)
-					).url
-				)
-			),
-			MessageType.video,
-			Mimetype.gif,
-			[caption],
-			caption
-		);
+			const buffer = await request.buffer(i).catch((e) => {
+				return void M.reply(e.message)
+		  })
+		while (true) {
+			try {
+				M.reply(
+					buffer || "Could not fetch image. Please try again later",
+					MessageType.image,
+					undefined,
+					undefined,
+					`🌟\n`,
+					undefined
+				).catch((e) => {
+					console.log(
+						`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`
+					);
+					// console.log('Failed')
+					M.reply(`Could not fetch image. Here's the URL: ${i}`);
+				});
+				break;
+			} catch (e) {
+				// console.log('Failed2')
+				M.reply(`Could not fetch image. Here's the URL : ${i}`);
+				console.log(
+					`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`
+				);
+			}
+		}
+		return void null;
 	};
 }
