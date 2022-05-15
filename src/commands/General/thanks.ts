7f565c9515e685c798918d6d740dbfb6ca6cb30f
minatosan89 <104668886+minatosan89@users.noreply.github.com>
2022-05-15 12:27:03 +0530

    Create thanks.ts

---
+++ src/commands/General/thanks.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "thanks",
+			description: "Generally used to thanks the bot (for a lame reason)",
+			category: "general",
+			usage: `${client.config.prefix}hi`,
+			baseXp: 10,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://c.tenor.com/GDCp-3tIs6sAAAPo/chitanda-eru.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `𝔂𝓸𝓾'𝓻𝓮 𝔀𝓮𝓵𝓬𝓸𝓶𝓮💕. \n`,
+			}
+		);
+	};
+}
